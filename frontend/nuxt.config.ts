21910d1739f1494d6d0b1a55dde788ddbb325e4f
greghori <e.souala@esi-sba.dz>
2025-08-16 22:53:59 +0100

    add tailwind css

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,14 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
-    '@nuxtjs/tailwindcss',
-    '@nuxt/icon',
-    '@vueuse/nuxt',
-    '@pinia/nuxt'
-  ]
+	compatibilityDate: "2025-07-15",
+	devtools: { enabled: true },
+	runtimeConfig: {
+		public: {
+			apiBase: process.env.NUXT_PUBLIC_API_BASE || "http://localhost:8000/api/v1",
+		},
+	},
+	modules: ["@nuxt/image", "@nuxt/test-utils", "@nuxt/ui", "@nuxt/icon", "@vueuse/nuxt", "@pinia/nuxt"],
+	css: ["~/assets/css/main.css"],
+	vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
 })
